b371b9d5a79cada6ff810b3fa563d031ff32b1b5
CandyWirus <amper123FS@gmail.com>
2023-07-04 15:16:10 -0600

    Create license.lua

---
+++ license.lua
@@ -0,0 +1,30 @@
+--Taxi Simulator Future Wall Driver Kit version %s
+--© 2023 Taxi Simulator Future. All rights reserved
+--THE TAXI SIMULATOR FUTURE WALL DRIVER KIT IS PROPRIETARY SOFTWARE!!!
+
+--[[
+	End User License Agreement:
+		Definitions:
+			"Kit": the Taxi Simulator Future Wall Driver Kit, and all of its parts, or any of its parts
+			"Game": any piece of software that uses the Kit, and all of its parts, or any of its parts
+			"Player": any person or persons interacting with a Game
+			"Currency": any real currency, or any virtual currency that can be exchanged for or bought with real currency in any way
+			"Profit": any acquisition of Currency
+			"Credits": any attribution by the Kit to any of its authors or affiliations
+		By using the Kit, you agree that:
+			Any Game making use of the Kit will be accessible to any person or persons, without the use of any Currency, and there will be no method to access the Game using any Currency
+			There will be no Profit made in any Game making use of the Kit, unless the Profit meets the following criteria:
+				The Profit is uncontrollable (e.g. Roblox Premium Payouts)
+				The Profit is the result of a donation that:
+					1) does not alter any Player's experience with the Game in any way
+					2) is only available for purchase outside of the Game itself
+					3) is not mentioned by the Game in any way
+			The Kit will not have its Credits hidden from the user in any way, nor deleted.
+			The copyright holder is not responsible in any way for any damage caused as a result of the use of the Kit, and you waive your right(s) to sue the copyright holder
+			The author(s) of any Game must comply with any request by the copyright holder to remove the Kit from their Game
+		The Kit comes with absolutely no warranty whatsoever
+--]]
+
+--To agree to the End User License Agreement, change the `eula` variable to `true`
+local eula = false
+return eula
